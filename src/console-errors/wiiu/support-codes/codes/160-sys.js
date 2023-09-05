d51ed3e01e7f1e2f17a5c57a71638ba23143da1a
R.G <61545240+Rix565@users.noreply.github.com>
2023-09-05 19:25:08 +0200

    Add 160-0103 error code data

--- src/console-errors/wiiu/support-codes/codes/160-sys.js
+++ src/console-errors/wiiu/support-codes/codes/160-sys.js
@@ -3,7 +3,7 @@ const SupportCode = require('../../../common/support-code');
 module.exports = {
 	'0101': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'0102': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
-	'0103': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
+	'0103': new SupportCode('Missing name', 'MLC Corruption error', 'There is a problem with the system memory.\nFor help, make a note of the error code and visit support.nintendo.com', 'Usually happens on 32gb Wii U models having a Hynix chip,\nthis chip has a problem that slowly corrupts its data ovetime,\nthere is nothing much to do except backing up the mlc\and replacing the Hynix chip entierly.', 'Missing link'),
 	'0104': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'0105': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'2706': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
