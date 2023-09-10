50ea586428245735d002683ea6e73aa8fc089698
R.G <61545240+Rix565@users.noreply.github.com>
2023-09-10 11:02:13 +0200

    Update 160-sys.js

--- src/console-errors/wiiu/support-codes/codes/160-sys.js
+++ src/console-errors/wiiu/support-codes/codes/160-sys.js
@@ -3,7 +3,7 @@ const SupportCode = require('../../../common/support-code');
 module.exports = {
 	'0101': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'0102': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
-	'0103': new SupportCode('Missing name', 'MLC corruption error', 'There is a problem with the system memory.\n\nFor help, make a note of the error code and visit\nsupport.nintendo.com', 'Usually happens on 32GB Wii U models having a Hynix chip,\nthis chip has a problem that slowly corrupts its data overtime,\nthere is nothing much to do except backing up the MLC\nand replacing the Hynix chip entierly.', 'Missing link'),
+	'0103': new SupportCode('Missing name', 'MLC corruption error', 'There is a problem with the system memory.\n\nFor help, make a note of the error code and visit\nsupport.nintendo.com', 'Usually happens on 32GB Wii U models having a Hynix chip,\nthis chip has a problem that slowly corrupts its data overtime,\nthere is nothing much to do except backing up the MLC\nand replacing the Hynix chip entierly.', 'https://github.com/GaryOderNichts/WiiUIdent'),
 	'0104': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'0105': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
 	'2706': new SupportCode('Missing name', 'Missing description', 'Missing message', 'Missing fix', 'Missing link'),
